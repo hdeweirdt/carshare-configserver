26887ac021c948876e42b0f1cce735a8b288319d
Harm De Weirdt <harm.deweirdt@mailfence.com>
2020-07-21 10:01:50 +0200

    initial commit

---
+++ src/main/java/be/harm/carshare/configserver/CarshareConfigserverApplication.java
@@ -0,0 +1,13 @@
+package be.harm.carshare.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshareConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarshareConfigserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/harm/carshare/configserver/CarshareConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package be.harm.carshare.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshareConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
